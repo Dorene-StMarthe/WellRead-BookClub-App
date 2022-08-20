267a0a8c13d12e0c4a7fb6d697d6c60296d2799d
Dorene07 <dstmarth@gmail.com>
2022-08-20 18:35:42 -0400

    Updated index and show routes, updated ejs files

---
+++ models/books.js
@@ -0,0 +1,35 @@
+//Array of books read
+const books = [ 
+    {
+        title: 'Roll of Thunder Hear My Cry', 
+        author: 'Mildred D. Taylor',
+        genre: 'Historical Fiction',
+        pages: '288'
+    },
+    {
+        title: 'The Fire Next Time', 
+        author: 'James Baldwin',
+        genre: 'Non-Fiction/Essay',
+        pages: '128'
+    },
+    {
+        title: 'Beloved',  
+        author:'Toni Morrison',
+        genre: 'Historical Fiction / Magical Realism',
+        pages: '324'
+    },
+    {
+        title: 'The Brief Wondrous Life of Oscar Wao', 
+        author: 'Junot Diaz',
+        genre: 'Domestic Fiction',
+        pages: '352'
+    },
+    {  
+        title: 'Americanah',
+        author: 'Chimamanda Ngozi Adichie',
+        genre: 'Fiction',
+        pages: '608'
+    }
+]
+
+module.exports = books
--- server.js
+++ server.js
@@ -1,52 +1,24 @@
 const express = require('express');
 const app= express();
 
-//Array of books read
-const books = [ 
-    {
-        title: 'Roll of Thunder Hear My Cry', 
-        author: 'Mildred D. Taylor',
-        genre: 'Historical Fiction',
-        pages: '288'
-    },
-    {
-        title: 'The Fire Next Time', 
-        author: 'James Baldwin',
-        genre: 'Non-Fiction/Essay',
-        pages: '128'
-    },
-    {
-        title: 'Beloved',  
-        author:'Toni Morrison',
-        genre: 'Historical Fiction / Magical Realism',
-        pages: '324'
-    },
-    {
-        title: 'The Brief Wondrous Life of Oscar Wao', 
-        author: 'Junot Diaz',
-        genre: 'Domestic Fiction',
-        pages: '352'
-    },
-    {  
-        title: 'Americanah',
-        author: 'Chimamanda Ngozi Adichie',
-        genre: 'Fiction',
-        pages: '608'
-    }
-]
-
-
+const books = require('./models/books.js')
+console.log(books)
 
 //index route
 app.get('/wellread', ( req, res )=>{
-    res.send(books);
+    res.render('index.ejs', {
+        allBooks: books
+    })
   });
 
   
   //show route
   app.get('/wellread/:indexOfBooksArray', (req, res) =>{
-    res.send(books[req.params.indexOfBooksArray])
-  })
+    res.render('show.ejs', {
+        book: books[req.params.indexOfBooksArray]
+    })
+});
+  
   
   
 
