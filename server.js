4c11f66410b970a8788541aaa2aa0c40bc364767
Dorene07 <dstmarth@gmail.com>
2022-08-20 19:41:12 -0400

    created new route and page directing to a form

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app= express();
 
 const books = require('./models/books.js')
-console.log(books)
+//console.log(books)
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 
 //index route
 app.get('/wellread', ( req, res )=>{
@@ -12,18 +18,30 @@ app.get('/wellread', ( req, res )=>{
   });
 
   
-  //show route
-  app.get('/wellread/:indexOfBooksArray', (req, res) =>{
+//new route
+app.get('/wellread/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+  
+//create POST route
+app.post('/wellread', (req, res) =>{
+    console.log(req.body)
+    books.push(req.body)
+    res.redirect('/wellread')
+})
+  
+  
+//show route
+app.get('/wellread/:indexOfBooksArray', (req, res) =>{
     res.render('show.ejs', {
         book: books[req.params.indexOfBooksArray]
     })
 });
-  
-  
-  
 
   
   app.listen(3000), () => {
     console.log('Server is listening!!')
+    
   }
 
