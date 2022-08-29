b735ffa03cf124856cd258a1bad5d3fd9a81f25f
Dorene07 <dstmarth@gmail.com>
2022-08-28 20:57:56 -0400

    user validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,16 @@ router.post('/register', (req, res) => {
     const salt = bcrypt.genSaltSync(10)
     req.body.password = bcrypt.hashSync(req.body.password, salt)
     console.log(req.body)
-})
 
+User.findOne({username: req.body.username}, (err, userExists) => {
+    if(userExists) {
+        res.send("that usernaem is taken")
+    } else {
+        User.create(req.body, (err, createdUser) => {
+            console.log(createdUser)
+            res.send('user created')
+          })
+ }
+})
+})
 module.exports = router
