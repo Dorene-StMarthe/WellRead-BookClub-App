1897f9460290b090695c78efdbce01732a0717b1
Dorene07 <dstmarth@gmail.com>
2022-08-28 20:32:29 -0400

    added bcrypt and post route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/users.js')
+
 const router = express.Router()
 
 router.get('/register', (req, res)=> {
     res.render('users/register.ejs')
+})
+
+router.post('/register', (req, res) => {
+    const salt = bcrypt.genSaltSync(10)
+    req.body.password = bcrypt.hashSync(req.body.password, salt)
+    console.log(req.body)
 })
 
 module.exports = router
