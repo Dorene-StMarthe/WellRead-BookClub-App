2283ed9ef00d5b1e94b64bf9d60d9ca2c0fd312d
Dorene07 <dstmarth@gmail.com>
2022-12-30 15:05:56 -0500

    added hyperlink to logo

--- models/books.js
+++ models/books.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const bookSchema= new Schema({
-    title: String,
-    author: String,
-    genre: String,
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    genre: {type: String, required: true},
     pages: {type: Number, default: 0},
-    review: String,
-    rating: {type: Number, default: 0},
+    review: {type: String, required: true},
+    rating: {type: Number, default: 0}, 
 });
 
 const Book = mongoose.model('Book', bookSchema)
