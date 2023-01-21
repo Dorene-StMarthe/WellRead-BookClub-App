63449e18904722d3356368612f0ed47e7383ba48
Dorene07 <dstmarth@gmail.com>
2023-01-21 16:47:53 -0500

    refactored user schema

--- models/users.js
+++ models/users.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: String
-})
+    username: {
+        type: String, 
+        required: true, 
+        unique: true,
+        maxlength: 100
+    },
+    password: {
+        type:String,
+        required: true,
+        minlength: 8
+    },
+    password2: {
+        type:String,
+        required: true,
+        minlength: 8
+    }
+});
 
 const User = mongoose.model('User' , userSchema)
 
